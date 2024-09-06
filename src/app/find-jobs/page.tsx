ead86d38d75e77d9c4644f5e0e12063de240f3be
Muhammad-Ilham-Ferdiansyah <ilhamferdiansyah737@gmail.com>
2024-09-06 14:34:23 +0700

    Add Slicing Find Job Page

---
+++ src/app/find-jobs/page.tsx
@@ -0,0 +1,7 @@
+import ExploreDataContainer from "@/containers/ExploreDataContainer";
+
+export default function FindJobsPage() {
+    return (
+       <ExploreDataContainer />
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,23 @@ import Client from "@/components/organisms/clients";
 import FeaturedJobs from "@/components/organisms/FeaturedJobs";
 import Hero from "@/components/organisms/hero";
 import LatestJob from "@/components/organisms/LatestJob";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="px-32 mb-10">
-      <Hero />
-      <Client />
-      <Category />
-      <BannerSignup />
-      <FeaturedJobs />
-      <LatestJob />
-    </div>
+    <>
+      <div className="absolute w-2/3 h-screen top-0 right-0 -z-10">
+            <Image src="/images/pattern.png" alt="/images/pattern.png" fill/>
+      </div>
+      <div className="px-32 mb-10">
+        <Hero />
+        <Client />
+        <Category />
+        <BannerSignup />
+        <FeaturedJobs />
+        <LatestJob />
+      </div>
+    
+    </>
   );
 }
