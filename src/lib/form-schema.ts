c0ec8d5de9face32e7d74102fd7cab966855081b
Muhammad-Ilham-Ferdiansyah <ilhamferdiansyah737@gmail.com>
2024-09-18 14:06:42 +0700

    Add Modal Dialog Apply Job

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -2,8 +2,19 @@ import { z }  from "zod"
 
 export const formFilterSchema = z.object({
     categories: z.array(z.string())
-})
+});
 
 export const formFilterCompanySchema = z.object({
     industry: z.array(z.string())
-})
+});
+
+export const formApplySchema = z.object({
+    resume: z.any().refine((file: any) => file?.name, 'Please upload Resume'),
+    fullname: z.string({required_error: 'Fullname is Required'}).min(5, {message: 'Full name min 5 character'}),
+    email: z.string({required_error: "Email is required"}).email({message: "Email not valid"}),
+    phone: z.string().min(6, {message: "Phone have min 6 characters"}),
+    previousJobTitle: z.string(),
+    linkedIn: z.string(),
+    portfolio: z.string(),
+    coverLetter: z.string(),
+});
