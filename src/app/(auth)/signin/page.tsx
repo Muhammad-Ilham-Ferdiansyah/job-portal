a4a1c253bf07162437049e9883c8383a83504aa8
Muhammad-Ilham-Ferdiansyah <ilhamferdiansyah737@gmail.com>
2024-09-23 17:56:37 +0700

    Integration SignIn

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -8,6 +8,9 @@ import Link from "next/link";
 import { FC } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { signIn } from "next-auth/react"
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 interface SignInPageProps {
     
@@ -18,8 +21,27 @@ const SignInPage: FC<SignInPageProps> = ({}) => {
         resolver: zodResolver(formSignInSchema)
     })
 
-    const onSubmit = (val: z.infer<typeof formSignInSchema>) => {
-        console.log(val);
+    const { toast } = useToast();
+
+    const router = useRouter();
+
+    const onSubmit = async (val: z.infer<typeof formSignInSchema>) => {
+        const authenticated = await signIn('credentials', {
+            ...val,
+            redirect: false
+        })
+
+        console.log(authenticated);
+
+        if (authenticated?.error){
+            toast({
+                title: "Error",
+                description: "Email or Password maybe wrong"
+            })
+            return;
+        }
+
+        router.push('/');
     }
 
     return ( 
